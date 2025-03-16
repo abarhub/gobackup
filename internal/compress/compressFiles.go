7b7df6749e1df91db6d9ae5243d3709ca75ca4f9
abarhub <abarhub@gmail.com>
2025-03-16 10:52:49 +0100

    suppression de calculComplet + nettoyage de code

--- internal/compress/compressFiles.go
+++ internal/compress/compressFiles.go
@@ -14,8 +14,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"regexp"
-	"slices"
 	"strings"
 	"time"
 )
@@ -37,18 +35,11 @@ func Compress(backup config.Backup, global config.BackupGlobal) (ResultatCompres
 
 	var complet bool
 	var date time.Time
-	if false {
-		log.Printf("calcul complet")
-		complet, date, err = calculComplet(repCompression, backup, global)
-		if err != nil {
-			return ResultatCompress{}, err
-		}
-	} else {
-		log.Printf("calcul complet2")
-		complet, date, err = calculComplet2(repCompression, backup, global)
-		if err != nil {
-			return ResultatCompress{}, err
-		}
+
+	log.Printf("calcul complet2")
+	complet, date, err = calculComplet2(repCompression, backup, global)
+	if err != nil {
+		return ResultatCompress{}, err
 	}
 
 	var listeFichierCompresse []string
@@ -111,169 +102,6 @@ func calculComplet2(repCompression string, backup config.Backup, global config.B
 	return listeFichiers.Calcul(repCompression, backup, global)
 }
 
-func calculComplet(repCompression string, backup config.Backup, global config.BackupGlobal) (bool, time.Time, error) {
-	repertoire := os.DirFS(repCompression)
-	return calculCompletFS(repertoire, backup.Nom, time.Now(), global.DebugCompression, global.NbBackupIncremental, ".")
-}
-
-func calculCompletFS(repertoire fs.FS, nomBackup string, now time.Time, debugCompression bool, nbBackupIncremental2 int, repCompression string) (bool, time.Time, error) {
-	files, err := fs.ReadDir(repertoire, repCompression)
-	if err != nil {
-		return false, time.Time{}, err
-	}
-	var liste []string
-
-	debutComplet := fmt.Sprintf("backupc_%v_", nomBackup)
-	debutIncrement := fmt.Sprintf("backupi_%v_", nomBackup)
-
-	log.Printf("Parcourt ...")
-	for _, file := range files {
-		//log.Printf("file: %v", file.Name())
-		if !file.IsDir() && (strings.HasPrefix(file.Name(), debutComplet) || strings.HasPrefix(file.Name(), debutIncrement)) {
-			s := file.Name()
-			//log.Printf("s001: %v", s)
-			for _, extCrypt := range []string{".gpg", ".age"} {
-				if strings.HasSuffix(s, extCrypt) {
-					s = strings.TrimSuffix(s, extCrypt)
-				}
-			}
-			//log.Printf("s002: %v", s)
-			if strings.HasSuffix(s, hashFiles.GetExtension()) {
-				s = strings.TrimSuffix(s, hashFiles.GetExtension())
-			}
-			//log.Printf("s003: %v", s)
-			var re = regexp.MustCompile(`\.[0-9]+$`)
-			s = re.ReplaceAllString(s, ``)
-			//log.Printf("s004: %v", s)
-			if strings.HasSuffix(s, ".7z") {
-				s = strings.TrimSuffix(s, ".7z")
-			}
-			//log.Printf("s: %v", s)
-			re2 := regexp.MustCompile("^(" + debutComplet + ")|(" + debutIncrement + `)[0-9]+_[0-9]+$`)
-			if re2.MatchString(s) {
-				//log.Printf("match: %v", s)
-				//s = trimStringFromString(s, "_")
-				if !slices.Contains(liste, s) {
-					//log.Printf("append: %v", s)
-					liste = append(liste, s)
-				}
-			}
-		}
-	}
-
-	if debugCompression {
-		log.Printf("trie de la liste %v ...", liste)
-	}
-	slices.SortFunc(liste,
-		func(a, b string) int {
-			var t01, t02 time.Time
-			a1 := strings.TrimPrefix(a, debutComplet)
-			a1 = strings.TrimPrefix(a1, debutIncrement)
-			b1 := strings.TrimPrefix(b, debutComplet)
-			b1 = strings.TrimPrefix(b1, debutIncrement)
-			s0 := a1[0:len(a1)-3] + "." + a1[len(a1)-3:]
-			tt, err0 := time.Parse("20060102_150405.000", s0)
-			if err0 != nil {
-				// erreur de parsing => on ignore le fichier
-				log.Printf("a=%s, a1=%s, s0=%s", a, a1, s0)
-				panic(err0)
-			} else {
-				t01 = tt
-			}
-			s0 = b1[0:len(b1)-3] + "." + a1[len(b1)-3:]
-			tt, err0 = time.Parse("20060102_150405.000", s0)
-			if err0 != nil {
-				// erreur de parsing => on ignore le fichier
-				log.Printf("b=%s, b1=%s, s0=%s", b, b1, s0)
-				panic(err0)
-			} else {
-				t02 = tt
-			}
-			//return cmp.Compare(t01, t02)
-			if t01 == t02 {
-				return 0
-			} else if t01.Before(t02) {
-				return -1
-			} else {
-				return 1
-			}
-		})
-
-	//sort.Sort(sort.StringSlice(liste))
-
-	if debugCompression {
-		log.Printf("liste sorted : %v", liste)
-	}
-
-	nbBackupIncremental := 0
-	var dateDebut time.Time
-	var dateDebutTrouve = false
-	var t1 time.Time
-	var backupComplet bool
-	if nbBackupIncremental2 > 0 {
-		for i := len(liste) - 1; i >= 0; i-- {
-			s := liste[i]
-			if debugCompression {
-				log.Printf("boucle %d : %s", i, s)
-			}
-			if !dateDebutTrouve {
-				var s0 string
-				if strings.HasPrefix(s, debutComplet) {
-					s0 = strings.TrimPrefix(s, debutComplet)
-				} else if strings.HasPrefix(s, debutIncrement) {
-					s0 = strings.TrimPrefix(s, debutIncrement)
-				}
-				if len(s0) == 18 {
-					s0 = s0[0:len(s0)-3] + "." + s0[len(s0)-3:]
-					if debugCompression {
-						log.Printf("s0 : %s", s0)
-					}
-					tt, err0 := time.Parse("20060102_150405.000", s0)
-					if err0 != nil {
-						// erreur de parsing => on ignore le fichier
-					} else {
-						dateDebutTrouve = true
-						dateDebut = tt
-					}
-				}
-			}
-			if debugCompression {
-				log.Printf("dateDebutTrouve : %v, dateDebut : %v", dateDebutTrouve, dateDebut)
-			}
-			if strings.HasPrefix(s, "backupc_") {
-				break
-			} else {
-				nbBackupIncremental++
-			}
-			if debugCompression {
-				log.Printf("nbBackupIncremental : %d", nbBackupIncremental)
-			}
-		}
-	}
-
-	log.Printf("date: %v (%v), nbBackupIncr: %d, conf nbBackupIncr: %d",
-		dateDebut, dateDebutTrouve, nbBackupIncremental, nbBackupIncremental2)
-
-	if nbBackupIncremental2 == 0 || nbBackupIncremental == 0 || nbBackupIncremental >= nbBackupIncremental2 {
-		backupComplet = true
-	} else {
-		backupComplet = false
-		if dateDebutTrouve {
-			t1 = time.Date(dateDebut.Year(), dateDebut.Month(), dateDebut.Day(), 0, 0, 0, 0, dateDebut.Location())
-		} else {
-			//now := time.Now()
-			t1 = time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
-		}
-	}
-
-	if debugCompression {
-		log.Printf("liste %v", liste)
-	}
-	log.Printf("backup complet: %v date: %v", backupComplet, t1)
-
-	return backupComplet, t1, nil
-}
-
 func compression(backup config.Backup, global config.BackupGlobal, fileList string, repCompression string, complet bool) ([]string, error) {
 	var program string
 	var args []string
@@ -302,7 +130,6 @@ func compression(backup config.Backup, global config.BackupGlobal, fileList stri
 
 		log.Printf("compression terminé")
 	} else {
-		//args = []string{"a", "-t7z", "-spf", "-bt", "-v1g", "-an", res}
 		data, err := f.ReadFile("data/archive_vide.7z")
 		if err != nil {
 			return []string{}, err
@@ -319,23 +146,23 @@ func compression(backup config.Backup, global config.BackupGlobal, fileList stri
 func listeFichiersCompresse(res string, repCompression string) ([]string, error) {
 	name := path.Base(res)
 
-	var listeFichiers []string
+	var listeFichiers2 []string
 
 	err := filepath.WalkDir(repCompression, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 		if !d.IsDir() && strings.HasPrefix(d.Name(), name) {
-			listeFichiers = append(listeFichiers, path)
+			listeFichiers2 = append(listeFichiers2, path)
 		}
 		return nil
 	})
 
-	log.Printf("liste dir: %v", listeFichiers)
+	log.Printf("liste dir: %v", listeFichiers2)
 
 	if err != nil {
 		return []string{}, err
 	} else {
-		return listeFichiers, nil
+		return listeFichiers2, nil
 	}
 }
